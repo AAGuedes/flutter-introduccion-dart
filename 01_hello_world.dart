5e15b740f9a59f20a90f450b10050e27da721d39
AAGuedes <airam.a.g.85@gmail.com>
2023-07-03 20:38:04 +0100

    Hola Mundo - Dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  //var myName = 'Airam';
+  //late final nyName = 'Airam';
+  //String myName = 'Airam';
+  const String myName = 'Airam';
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 2 * 3}');
+}
