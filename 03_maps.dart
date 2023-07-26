28698ac44ba4b0258a5e247978f0527c739065b4
AAGuedes <airam.a.g.85@gmail.com>
2023-07-26 12:46:22 +0100

    Maps

---
+++ 03_maps.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+  final Map<String, dynamic> pokemon = {
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlive': true,
+    'abilities': <String>['impostor'],
+    'sprites': <int, String>{
+      1: 'ditto/front.png',
+      2: 'ditto/back.png'
+    }
+  };
+  
+  print(pokemon);
+  
+  print('Name: ${ pokemon['name'] }');
+  
+  print('Sprites: ${ pokemon['sprites'] }');
+  
+  // Tarea - Imprimir de sprites el front y el back
+  print('Back: ${ pokemon['sprites'][2] }');
+
+  print('Front: ${ pokemon['sprites'][1] }');
+  
+}
