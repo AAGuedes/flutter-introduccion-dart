db7aee8925e9124c4365e25b045ddb126457725b
AAGuedes <airam.a.g.85@gmail.com>
2023-08-01 00:22:47 +0100

    Parametros con nombre

--- 05_functions.dart
+++ 05_functions.dart
@@ -3,6 +3,8 @@ void main () {
   print(greetEveryone());
   
   print ('Suma: ${addTwoNumbers(10,20)}');
+  
+  print(greetPerson(name : 'Airam', message: 'Hi'));
 }
 
 String greetEveryone() => 'Hello everyone!';
@@ -10,8 +12,10 @@ String greetEveryone() => 'Hello everyone!';
 int addTwoNumbers(int a, int b) => a + b;
 
 int addTwoNumbersOptional(int a, [int b = 0]) {
-
-  b = b ?? 0;
-
+//  b = b ?? 0;
   return a + b;
+}
+
+String greetPerson({required String name, String message = 'Hola'}) {
+  return '$message, $name';
 }
