6f2865af075a8fc0edc214889a110343b3f69ed0
AAGuedes <airam.a.g.85@gmail.com>
2023-08-03 23:22:02 +0100

    Aserciones

--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -1,8 +1,8 @@
 void main() {
   
-  final mySquare = Square(side: 10);
+  final mySquare = Square(side: -10);
   
-  mySquare.side = -5;
+//  mySquare.side = -5;
 
   print('Area: ${mySquare.area}');
 }
@@ -12,7 +12,8 @@ class Square {
   double _side; //side * side
   
   Square({required double side})
-    : _side = side;
+    : assert(side >= 0, 'side must be greater then 0'),
+      _side = side;
   
   double get area {
     return _side * _side;
@@ -21,7 +22,7 @@ class Square {
   set side(double value) {
     print('Setting new value $value');
 
-    if(value < 0) throw 'Value must be greater then 0';
+    if(value < 0) throw 'value must be greater then 0';
 
     _side = value;
   }
